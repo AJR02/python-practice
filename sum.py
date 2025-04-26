d9bba81e9e943d9f578007b8ed4f6b3b6e659003
AJR02 <AJR02@user.noreply.github.com>
2025-04-26 14:57:31 +0300

    task1:sum

---
+++ sum.py
@@ -0,0 +1,8 @@
+#sum of all elements in a list
+#Create a list called elements that will contain numbers
+elements=[21,28,35,42,49]
+total=0
+#Calculate by calling the sum function then assign it to the total variable
+total=sum(elements)
+#print the value of total
+print(total)
